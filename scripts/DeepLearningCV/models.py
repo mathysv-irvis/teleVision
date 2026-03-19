2305de726239be033f5a15db209a5afa6db8e267
MathysVinatier <vinmat2002@gmail.com>
2026-03-19 18:07:51 +0900

    modifying Net architecture

--- scripts/DeepLearningCV/models.py
+++ scripts/DeepLearningCV/models.py
@@ -30,13 +30,19 @@ class TinyNet(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.name = "tinynet"
-        self.conv = nn.Conv2d(3,16,3,padding=1)
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(16, num_classes)
+
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self,x):
-        x = F.relu(self.conv(x))
-        x = self.pool(x)
-        x = torch.flatten(x,1)
-        return self.fc(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return  self.fc3(x)
 
